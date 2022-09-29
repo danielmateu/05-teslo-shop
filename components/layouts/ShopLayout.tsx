f02d2d06d3c5a2b5f7f6756da148de0e20c01478
Daniel Mateu <danielmateu86@gmail.com>
2022-09-29 19:27:25 +0200

    MEnu lateral creado

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import { FC } from "react";
-import { Navbar } from "../ui";
+import { Navbar, SideMenu } from "../ui";
 
 
 interface Props {
@@ -37,7 +37,7 @@ export const ShopLayout: FC<Props> = ({ children, title, pageDescription, imageF
                 <Navbar/>
             </nav>
 
-            {/* TODO: SideBar */}
+            <SideMenu />
 
             <main style={{
                 margin: '80px auto',
