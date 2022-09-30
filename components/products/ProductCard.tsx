22d32ef104b829018d2b4398e0130383d3e192ac
Daniel Mateu <danielmateu86@gmail.com>
2022-09-30 10:07:32 +0200

    Pantalla de producto actualizada

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,11 +1,16 @@
-import { Grid, Card, CardActionArea, CardMedia, Typography } from "@mui/material"
-import { Box } from "@mui/system";
 import { FC, useMemo, useState } from "react"
+import NextLink from 'next/link';
+
+import { Grid, Card, CardActionArea, CardMedia, Typography, Link } from "@mui/material"
+import { Box } from "@mui/system";
 import { IProduct } from "../../interfaces"
+
+
 
 interface Props {
     product: IProduct;
 }
+
 
 
 export const ProductCard: FC<Props> = ({ product }) => {
@@ -14,25 +19,30 @@ export const ProductCard: FC<Props> = ({ product }) => {
 
     const productImage = useMemo(() => {
         return isHovered ? `products/${product.images[1]}` : `products/${product.images[0]}`
-    },[isHovered, product.images])
+    }, [isHovered, product.images])
 
     return (
-        <Grid item 
-            xs={6} 
+        <Grid item
+            xs={6}
             sm={4}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
             <Card>
-                <CardActionArea>
-                    <CardMedia
-                        component='img'
-                        className="fadeIn"
-                        image={productImage}
-                        alt={product.title}
-                        onLoad={() => console.log('Ya cargado') }
-                    />
-                </CardActionArea>
+
+                <NextLink href='/product/slug' passHref prefetch={false}>
+                    <Link>
+                        <CardActionArea>
+                            <CardMedia
+                                component='img'
+                                className="fadeIn"
+                                image={productImage}
+                                alt={product.title}
+                                onLoad={() => console.log('Ya cargado')}
+                            />
+                        </CardActionArea>
+                    </Link>
+                </NextLink>
 
                 <Box sx={{ p: 2 }} className='fadeIn'>
                     <Typography fontWeight={700}>{product.title}</Typography>
---
+++ pages/product/slug.tsx
@@ -0,0 +1,50 @@
+import { Button, Chip, Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react'
+import { ShopLayout } from '../../components/layouts'
+import { initialData } from '../../database/products'
+
+const product = initialData.products[0];
+
+const ProductPage = () => {
+  return (
+    <ShopLayout title={`${product.title}`} pageDescription={`${product.description}`}>
+      <Grid container spacing={3}>
+
+        <Grid item xs={12} sm={7}>
+          {/* SlideShow */}
+        </Grid>
+
+        <Grid item xs={12} sm={5}>
+          <Box display='flex' flexDirection='column'>
+            
+            {/* Titulos */}
+            <Typography variant='h1' component='h1'>{product.title}</Typography>
+            <Typography variant='subtitle1' component='h2'>{`€${product.price}`}</Typography>
+
+            {/* Cantidad */}
+            <Box sx = {{my:2}}>
+              <Typography variant='subtitle2' component='h3'>Cantidad</Typography>
+            </Box>
+
+            {/* Agregar al carrito */}
+
+            <Button color='secondary' className='circular-btn'>Agregar al carrito</Button>
+
+            {/* <Chip label='Sin disponibilidad' color='error' variant='outlined'/> */}
+
+
+            {/* Description */}
+            <Box sx = {{mt: 2}}>
+              <Typography variant='subtitle2'>Descripción</Typography>
+              <Typography variant='body2'>{product.description}</Typography>
+            </Box>
+          </Box> 
+        </Grid>
+
+      </Grid>
+    </ShopLayout>
+  )
+}
+
+export default ProductPage
