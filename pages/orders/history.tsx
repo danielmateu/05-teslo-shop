ff74fb39f14682f115f51e57665c5c7a04999991
Daniel Mateu <danielmateu86@gmail.com>
2022-10-01 10:01:01 +0200

    instalación @mui/x-data-grid

---
+++ pages/orders/history.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ShopLayout } from '../../components/layouts'
+
+const HistoryPage = () => {
+  return (
+    <ShopLayout title="Historial de pedidos" pageDescription='Historial de pedidos del cliente'>
+        
+    </ShopLayout>
+  )
+}
+
+export default HistoryPage
