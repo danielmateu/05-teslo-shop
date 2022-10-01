03ff2dc2185281617dc159e7e5544b29bc4974cb
Daniel Mateu <danielmateu86@gmail.com>
2022-10-01 23:46:45 +0200

    Creación pagina de login e inicio de sesión

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,13 +1,45 @@
-import { Box } from '@mui/system'
-import React from 'react'
 import { AuthLayout } from '../../components/layouts'
+
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import NextLink from 'next/link';
+
 
 const LoginPage = () => {
   return (
     <AuthLayout title="Ingresar">
-        <Box >
+      <Box sx={{ width: 350, padding: '15em 1em' }} >
 
-        </Box>
+        <Grid container spacing={2}  >
+          <Grid item>
+            <Typography variant='h1' component='h1'>Iniciar Sesión</Typography>
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField label='Correo' variant='filled' fullWidth/>
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField label='Contraseña' type='password' variant='filled' fullWidth/>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button color='secondary' className='circular-btn' size='large' fullWidth>Ingresar
+            </Button>
+          </Grid>
+
+          <Grid item xs={12} display = 'flex' justifyContent='end'>
+            <NextLink href='/auth/register' passHref>
+              <Link underline="always">
+              ¿Aun no estás registrad@?
+              </Link>
+            </NextLink>
+          </Grid>
+
+          
+
+        </Grid>
+      </Box>
 
 
     </AuthLayout>
---
+++ pages/auth/register.tsx
@@ -0,0 +1,59 @@
+
+
+import { AuthLayout } from '../../components/layouts'
+
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import NextLink from 'next/link';
+
+
+const RegisterPage = () => {
+    return (
+        <AuthLayout title="Página de registro">
+            <Box sx={{ width: 350, padding: '15em 1em' }} >
+
+                <Grid container spacing={2}  >
+                    <Grid item>
+                        <Typography variant='h1' component='h1'>Crea tu cuenta</Typography>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <TextField label='Nombre' variant='filled' fullWidth />
+                    </Grid>
+                    
+                    <Grid item xs={12}>
+                        <TextField label='Correo' variant='filled' fullWidth />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <TextField label='Contraseña' type='password' variant='filled' fullWidth />
+                    </Grid>
+
+                    {/* <Grid item xs={12}>
+                        <TextField label='Repita contraseña' type='password' variant='filled' fullWidth />
+                    </Grid> */}
+
+                    <Grid item xs={12}>
+                        <Button color='secondary' className='circular-btn' size='large' fullWidth>Ingresar
+                        </Button>
+                    </Grid>
+
+                    <Grid item xs={12} display='flex' justifyContent='end'>
+                        <NextLink href='/auth/login' passHref>
+                            <Link underline="always">
+                                ¿Ya estás registrad@?
+                            </Link>
+                        </NextLink>
+                    </Grid>
+
+
+
+                </Grid>
+            </Box>
+
+
+        </AuthLayout>
+    )
+}
+
+export default RegisterPage
