6ba1b24531571a97e6daa36526fa1c17fa5a1475
Daniel Mateu <danielmateu86@gmail.com>
2022-10-02 20:13:33 +0200

    Creación de Custom Hooks y configuracion global de SWR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,27 @@
 import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
+import { CssBaseline } from '@mui/material'
+import useSWR, { SWRConfig } from 'swr'
+
 import { ThemeProvider } from '@mui/system'
 import { lightTheme } from '../themes'
-import { CssBaseline } from '@mui/material'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+
+    <SWRConfig 
+    value={{
+      fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+    }}
+  >
     <ThemeProvider theme={lightTheme}>
       <CssBaseline />
-
-
       <Component {...pageProps} />
     </ThemeProvider>
+    
+  </SWRConfig>
+    
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,29 +4,26 @@ import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material
 import { ShopLayout } from '../components/layouts'
 // import { initialData } from '../database/products';
 import { ProductList } from '../components/products';
+import { useProducts } from '../hooks';
 
-import useSWR from "swr"
 
-const fetcher = (...args: [key:string]) => fetch(...args).then(res => res.json())
 
 
 const HomePage: NextPage = () => {
   
-  const { data, error } = useSWR("/api/products", fetcher,{
-    refreshInterval: 1000,
-  });
 
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-
-  console.log({data})
+  const {products, isLoading} = useProducts('/products');
 
   return (
     <ShopLayout title={'Mundocamper - Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
       <Typography variant="h1" component='h1' >Tienda</Typography>
       <Typography variant="h2" sx={{ mb: 1 }} >Todos los productos</Typography>
 
-      <ProductList products={data} />
+      {
+        isLoading ? <h1>Cargando...</h1> : <ProductList products={products} />
+      }
+
+      
     </ShopLayout>
   )
 }
