d91b0daf54e5a84521810a8ad26ea8e4c752e100
Daniel Mateu <danielmateu86@gmail.com>
2022-10-02 19:51:39 +0200

    Instalación de SWR - React Hooks for data Fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,33 @@ import type { NextPage } from 'next'
 import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 
 import { ShopLayout } from '../components/layouts'
-import { initialData } from '../database/products';
+// import { initialData } from '../database/products';
 import { ProductList } from '../components/products';
 
+import useSWR from "swr"
 
-const Home: NextPage = () => {
+const fetcher = (...args: [key:string]) => fetch(...args).then(res => res.json())
+
+
+const HomePage: NextPage = () => {
+  
+  const { data, error } = useSWR("/api/products", fetcher,{
+    refreshInterval: 1000,
+  });
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  console.log({data})
+
   return (
     <ShopLayout title={'Mundocamper - Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
       <Typography variant="h1" component='h1' >Tienda</Typography>
       <Typography variant="h2" sx={{ mb: 1 }} >Todos los productos</Typography>
 
-      <ProductList products={initialData.products as any} />
+      <ProductList products={data} />
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
