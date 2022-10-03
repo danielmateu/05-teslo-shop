496831c7a49c0a2b6c41dd8b52814dbadd1c4141
Daniel Mateu <danielmateu86@gmail.com>
2022-10-03 11:25:51 +0200

    Creación del UiContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,22 +6,27 @@ import useSWR, { SWRConfig } from 'swr'
 
 import { ThemeProvider } from '@mui/system'
 import { lightTheme } from '../themes'
+import { UiProvider } from '../context'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
 
-    <SWRConfig 
-    value={{
-      fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-    }}
-  >
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-    
-  </SWRConfig>
-    
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <UiProvider>
+
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UiProvider>
+
+
+    </SWRConfig>
+
   )
 }
 
