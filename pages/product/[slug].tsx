5f80ef679977826b09343720e17801c1065dc688
Daniel Mateu <danielmateu86@gmail.com>
2022-10-05 13:45:04 +0200

    Estilando card y cart si un articulo está o no disponible

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -14,7 +14,7 @@ interface Props {
 }
 
 
-const ProductPage:NextPage<Props> = ({ product }) => {
+const ProductPage: NextPage<Props> = ({ product }) => {
 
   // const router = useRouter();
   // const { products: product, isLoading } = useProducts(`/products/${ router.query.slug }`);
@@ -22,45 +22,53 @@ const ProductPage:NextPage<Props> = ({ product }) => {
 
 
   return (
-    <ShopLayout title={ product.title } pageDescription={ product.description }>
-    
+    <ShopLayout title={product.title} pageDescription={product.description}>
+
       <Grid container spacing={3}>
 
-        <Grid item xs={12} sm={ 7 }>
-          <ProductSlideshow 
-            images={ product.images }
+        <Grid item xs={12} sm={7}>
+          <ProductSlideshow
+            images={product.images}
           />
         </Grid>
 
-        <Grid item xs={ 12 } sm={ 5 }>
+        <Grid item xs={12} sm={5}>
           <Box display='flex' flexDirection='column'>
 
             {/* titulos */}
-            <Typography variant='h1' component='h1'>{ product.title }</Typography>
-            <Typography variant='subtitle1' component='h2'>{ `$${product.price}` }</Typography>
+            <Typography variant='h1' component='h1'>{product.title}</Typography>
+            <Typography variant='subtitle1' component='h2'>{`$${product.price}`}</Typography>
 
             {/* Cantidad */}
             <Box sx={{ my: 2 }}>
               <Typography variant='subtitle2'>Cantidad</Typography>
               <ItemCounter />
-              <SizeSelector 
+              <SizeSelector
                 // selectedSize={ product.sizes[2] } 
-                sizes={ product.sizes }
+                sizes={product.sizes}
               />
             </Box>
 
 
             {/* Agregar al carrito */}
-            <Button color="secondary" className='circular-btn'>
-              Agregar al carrito
-            </Button>
+            {
+              (product.inStock > 0)
+                ? (
+                  <Button color="secondary" >
+                    Agregar al carrito
+                  </Button>
+                )
+                :
+                (
+                <Chip label="No hay stock" color="error" variant='outlined' />
+                )
+            }
 
-            {/* <Chip label="No hay disponibles" color="error" variant='outlined' /> */}
 
             {/* Descripción */}
-            <Box sx={{ mt:3 }}>
+            <Box sx={{ mt: 3 }}>
               <Typography variant='subtitle2'>Descripción</Typography>
-              <Typography variant='body2'>{ product.description }</Typography>
+              <Typography variant='body2'>{product.description}</Typography>
             </Box>
 
           </Box>
@@ -79,18 +87,18 @@ const ProductPage:NextPage<Props> = ({ product }) => {
 // - Only if you need to pre-render a page whose data must be fetched at request time
 //* No usar esto.... SSR
 // export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  
+
 //   const { slug = '' } = params as { slug: string };
 //   const product = await dbProducts.getProductBySlug( slug );
 
-  // if ( !product ) {
-  //   return {
-  //     redirect: {
-  //       destination: '/',
-  //       permanent: false
-  //     }
-  //   }
-  // }
+// if ( !product ) {
+//   return {
+//     redirect: {
+//       destination: '/',
+//       permanent: false
+//     }
+//   }
+// }
 
 //   return {
 //     props: {
@@ -103,12 +111,12 @@ const ProductPage:NextPage<Props> = ({ product }) => {
 // getStaticPaths....
 // You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
-  
+
   const productSlugs = await dbProducts.getAllProductSlugs();
 
-  
+
   return {
-    paths: productSlugs.map( ({ slug }) => ({
+    paths: productSlugs.map(({ slug }) => ({
       params: {
         slug
       }
@@ -123,11 +131,11 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 //- The data can be publicly cached (not user-specific).
 //- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  
-  const { slug = '' } = params as { slug: string };
-  const product = await dbProducts.getProductBySlug( slug );
 
-  if ( !product ) {
+  const { slug = '' } = params as { slug: string };
+  const product = await dbProducts.getProductBySlug(slug);
+
+  if (!product) {
     return {
       redirect: {
         destination: '/',
