2c33fbc79cd9c057f4ce73babc95829ace57c626
Daniel Mateu <danielmateu86@gmail.com>
2022-10-07 10:37:11 +0200

    Validando formulario de login en el front end

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import { AuthLayout } from '../../components/layouts'
+import { validations } from '../../utils';
 
 type FormData = {
   email: string,
@@ -14,14 +15,16 @@ type FormData = {
 const LoginPage = () => {
 
   const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  console.log({ errors });
+
 
   const onLoginUser = (data: FormData) => {
     console.log(data);
   }
 
   return (
-    <AuthLayout title="Ingresar">
-      <form onSubmit={handleSubmit(onLoginUser)}>
+    <AuthLayout title={"Ingresar"}>
+      <form onSubmit={handleSubmit(onLoginUser)} noValidate>
         <Box sx={{ width: 350, padding: '12em 1em' }} >
 
           <Grid container spacing={2}  >
@@ -31,20 +34,32 @@ const LoginPage = () => {
 
             <Grid item xs={12}>
               <TextField
-              type={'email'}
-              label='Correo' 
-              variant='filled' 
-              fullWidth
-              {...register('email')} />
+                type={'email'}
+                label='Correo'
+                variant='filled'
+                fullWidth
+                {
+                ...register('email', {
+                  required: 'Este campo es obligatorio',
+                  validate: (val) => validations.isEmail(val)
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
             </Grid>
 
             <Grid item xs={12}>
-              <TextField 
-              label='Contraseña' 
-              type='password' 
-              variant='filled' 
-              fullWidth
-              {...register('password')} 
+              <TextField
+                label='Contraseña'
+                type='password'
+                variant='filled'
+                fullWidth
+                {...register('password',{
+                  required: 'Este campo es requerido',
+                  minLength: {value: 6, message: 'Mínimo 6 caracteres'}
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
               />
             </Grid>
 
