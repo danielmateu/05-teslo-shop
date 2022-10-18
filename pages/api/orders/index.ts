26ccf168b875e62a49c460c9c30e665cb1bdeeae
Daniel Mateu <danielmateu86@gmail.com>
2022-10-18 19:27:39 +0200

    Revisar el metodo createOrder...

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -2,11 +2,11 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
 import { db } from '../../../database'
 import { IOrder } from '../../../interfaces'
-import { Product } from '../../../models'
+import { Order, Product } from '../../../models'
 
-type Data = {
-    message: string
-}
+type Data = 
+| {message: string}
+| IOrder
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
@@ -45,10 +45,10 @@ const createOrder = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
     // console.log(dbProducts);
 
     try {
-        
+
         const subTotal = orderItems.reduce((prev, current) =>{
 
-            const currentPrice = dbProducts.find(prod => prod._id === current._id)!.price;
+            const currentPrice = dbProducts.find(prod => prod.id === current._id)?.price;
             if(!currentPrice){
                 throw new Error("Verifique el carrito de nuevo, producto no existente");
             }
@@ -56,12 +56,31 @@ const createOrder = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
             return ( currentPrice * current.quantity ) + prev
         } , 0);
 
+        const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
 
+        const backendTotal = subTotal * ( taxRate + 1);
 
-    } catch (error) {
-        
+        if(total !== backendTotal ) {
+            throw new Error("El total no cuadra con el monto");
+        }
+
+        //Todo bien hasta este punto
+        const userId = session.user._id;
+        const newOrder = new Order({...req.body, isPaid: false, user: userId});
+
+        await newOrder.save();
+
+        return res.status(201).json(newOrder);
+
+    } catch (error:any) {
+        await db.disconnect();
+        console.log(error);
+
+        res.status(400).json({ 
+            message: error.message || 'Revise logs del servidor'
+        })
     }
 
 
-    return res.status(201).json(req.body);
+    // return res.status(201).json(req.body);
 }
